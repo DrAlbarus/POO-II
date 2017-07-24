8c059fe7737b6a318d2337351d79f0b9aed7754f
DrAlbarus <axfilipe777@gmail.com>
2017-07-24 05:32:34 -0500

    Classe Garcom

---
+++ src/restaurante/Garcom.java
@@ -0,0 +1,33 @@
+package restaurante;
+import java.time.LocalDate;
+
+public class Garcom 
+{
+    private String nome;
+    private Double salario;
+    
+    public String getNome()
+    {
+        return nome;
+    }
+    
+    public void setNome(String nome)
+    {
+        this.nome = nome;
+    }
+
+    public Double getSalario()
+    {
+        return salario;
+    }
+
+    public void setSalario(Double salario)
+    {
+        this.salario = salario;
+    }
+
+    void calculaSalario()
+    {
+        
+    }
+}
